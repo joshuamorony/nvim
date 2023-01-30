5740d01fe8d7e525102446cda37b0a2a179a4726
Joshua Morony <joshua.morony@gmail.com>
2023-01-31 09:42:44 +1030

    update

--- init.lua
+++ init.lua
@@ -11,16 +11,3 @@ else
 	require("settings")
 	require("transparent")
 end
-
-
--- call plug#begin()
--- Plug 'patstockwell/vim-monokai-tasty'
--- Plug 'HerringtonDarkholme/yats.vim'
--- Plug 'pangloss/vim-javascript'
--- Plug 'MaxMEllon/vim-jsx-pretty'
--- Plug 'styled-components/vim-styled-components'
--- Plug 'elzr/vim-json'
--- Plug 'jparise/vim-graphql'
--- call plug#end()
-
-
