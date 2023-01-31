2f942bb5e127ef4d63cc871cd4538164040a7338
Joshua Morony <joshua.morony@gmail.com>
2023-01-31 10:44:06 +1030

    packer and settings

---
+++ lua/josh/packer.lua
@@ -0,0 +1,68 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    -- for easily changing a line to comment
+    use "preservim/nerdcommenter"
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.0',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+
+    use { "ellisonleao/gruvbox.nvim" }
+    use('Mofiqul/vscode.nvim')
+
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use('nvim-treesitter/playground')
+
+    -- a fork of nvim-treesitter that fixes inline html
+    use({ "elgiano/nvim-treesitter-angular", branch = "topic/jsx-fix" })
+
+    use('ThePrimeagen/harpoon')
+    use('mbbill/undotree')
+    use('kdheepak/lazygit.nvim')
+    -- lsp stuffs
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },
+            { 'rafamadriz/friendly-snippets' },
+        }
+    }
+    use('jose-elias-alvarez/null-ls.nvim')
+    use('MunifTanjim/prettier.nvim')
+    use {
+        'nvim-tree/nvim-tree.lua',
+        -- latest has a bug with FindFileToggle. Use this until that resolves
+        commit = '16f2806d5968157fd6f76542c9ac358c684a3a03',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        },
+    }
+    use('windwp/nvim-autopairs')
+    use('windwp/nvim-ts-autotag')
+    use {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+    }
+    use('norcalli/nvim-colorizer.lua')
+
+end)
---
+++ lua/josh/settings.lua
@@ -0,0 +1,41 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "120"
+
+vim.g.mapleader = " "
+
+-- Add asterisks in block comments
+vim.opt.formatoptions:append { 'r' }
+
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd [[
+  augroup highlight_yank
+  autocmd!
+  au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=100})
+  augroup END
+]]
