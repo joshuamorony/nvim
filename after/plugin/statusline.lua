1702191ab5c1cb3ac832442a944bd6b3694ed57e
Joshua Morony <joshua.morony@gmail.com>
2023-02-03 09:52:54 +1030

    move diagnostics

--- after/plugin/statusline.lua
+++ after/plugin/statusline.lua
@@ -7,27 +7,28 @@ lualine.setup({
 	sections = {
 		lualine_a = { "" },
 		lualine_b = { "branch" },
-		lualine_c = { "filename" },
+		lualine_c = { "filename",
+            {
+                "diagnostics",
+                sources = { "nvim_diagnostic" },
+                symbols = { error = " ", warn = " ", info = " " },
+                colored = true,
+                diagnostics_color = {
+                    error = "DiagnosticError",
+                    warn = "DiagnosticWarn",
+                    info = "DiagnosticInfo",
+                    hint = "DiagnosticHint",
+                }
+            },
+        },
 		lualine_x = { { gps.get_location, cond = gps.is_available } },
 		-- lualine_x = {"b:gitsigns_status"},
 		lualine_y = { "filetype" },
 		lualine_z = {
 			"location",
-			{
-				"diagnostics",
-				sources = { "nvim_diagnostic" },
-				symbols = { error = " ", warn = " ", info = " " },
-				colored = true,
-				diagnostics_color = {
-					error = "DiagnosticError",
-					warn = "DiagnosticWarn",
-					info = "DiagnosticInfo",
-					hint = "DiagnosticHint",
-				},
-			},
             {
                 "progress"
-            }
+            },
 		},
 	},
 })
