891ffcdc536f1b80d6f9c31b771a7ba3039c0638
Joshua Morony <joshua.morony@gmail.com>
2023-02-06 09:47:30 +1030

    configure harpoon

--- lua/josh/keymaps.lua
+++ lua/josh/keymaps.lua
@@ -12,12 +12,10 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.LazyGit)
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-vim.keymap.set("n", "<C-h>", function () ui.nav_file(1) end)
-vim.keymap.set("n", "<C-t>", function () ui.nav_file(2) end)
-vim.keymap.set("n", "<C-n>", function () ui.nav_file(3) end)
-vim.keymap.set("n", "<C-s>", function () ui.nav_file(4) end)
+vim.keymap.set("n", "<leader>ha", mark.add_file)
+vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>hl", function () ui.nav_next() end)
+vim.keymap.set("n", "<leader>hh", function () ui.nav_next() end)
 
 -- undotree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
