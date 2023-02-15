599c2f0a367e226263d9666bb0cf5b746af4a4e5
Joshua Morony <joshua.morony@gmail.com>
2023-02-15 11:53:26 +1030

    add git_files keymap

--- lua/josh/keymaps.lua
+++ lua/josh/keymaps.lua
@@ -53,6 +53,7 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
+vim.keymap.set('n', '<leader>ss', require('telescope.builtin').git_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
