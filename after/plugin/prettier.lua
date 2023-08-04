b6a9551452d51f3e0e93f5694bfe0d8cd0da2453
Joshua Morony <joshua.morony@gmail.com>
2023-08-04 14:37:07 +0930

    dont use custom formatter

--- after/plugin/prettier.lua
+++ after/plugin/prettier.lua
@@ -1,9 +1,9 @@
 local null_ls = require("null-ls")
 
-local format = require('josh.custom.lsp.format')
+local format = require("josh.custom.lsp.format")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 require("null-ls").setup({
-  sources = {
+	sources = {
 		null_ls.builtins.formatting.prettierd,
 		null_ls.builtins.formatting.stylua,
 		null_ls.builtins.formatting.stylelint,
@@ -13,40 +13,40 @@ require("null-ls").setup({
 		null_ls.builtins.diagnostics.cspell.with({
 			filetypes = { "markdown", "tex", "text", "svx" },
 		}),
-  },
-    -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-                    format({ async = false})
-                end,
-            })
-        end
-    end,
+	},
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+		end
+	end,
 })
 
 local prettier = require("prettier")
 
 prettier.setup({
-    bin = 'prettierd', -- or `'prettierd'` (v0.22+)
-    filetypes = {
-        "css",
-        "html",
-        "javascript",
-        "javascriptreact",
-        "json",
-        "markdown",
-        "scss",
-        "typescript",
-        "typescriptreact",
-        "yaml",
-        "lua",
-        "svelte"
-    },
+	bin = "prettierd", -- or `'prettierd'` (v0.22+)
+	filetypes = {
+		"css",
+		"html",
+		"javascript",
+		"javascriptreact",
+		"json",
+		"markdown",
+		"scss",
+		"typescript",
+		"typescriptreact",
+		"yaml",
+		"lua",
+		"svelte",
+	},
 })
