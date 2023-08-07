b84e360a8960538d4931f40fbc9f34410baabc88
Joshua Morony <joshua.morony@gmail.com>
2023-08-08 09:07:34 +0930

    configure snippets

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -8,18 +8,15 @@ require("luasnip/loaders/from_vscode").lazy_load()
 require("luasnip/loaders/from_vscode").lazy_load({ paths = { "~/.config/nvim/lua/josh/snippets/" } })
 
 ls.config.set_config({
-	history = true,
+	history = false,
 
 	updateevents = "TextChanged, TextChangedI",
 
-	enable_autosnippets = true,
+	enable_autosnippets = false,
 })
 
 vim.keymap.set({ "i" }, "<C-K>", function()
-	ls.expand()
-end, { silent = true })
-vim.keymap.set({ "i", "s" }, "<C-L>", function()
-	ls.jump(1)
+	ls.expand_or_jump()
 end, { silent = true })
 vim.keymap.set({ "i", "s" }, "<C-J>", function()
 	ls.jump(-1)
